4ad95800373b5dbf1d546f3f68cd112e6aa7a901
Chris Perivolaropoulos <cperivol@csail.mit.edu>
2016-07-25 18:07:54 +0100

    New getmail

--- getmail.sh
+++ getmail.sh
@@ -1,43 +1,42 @@
 #!/bin/bash
 # $ crontab -e
-# */5 * * * * /Users/drninjabatman/bin/getmail.sh >/tmp/stdout.log 2>/tmp/stderr.log
+# */5 * * * * NOTIFY=y /Users/drninjabatman/bin/getmail.sh >/tmp/stdout.log 2>/tmp/stderr.log
 set -e
 mbsync="/usr/local/bin/mbsync"
 notify=/usr/local/bin/terminal-notifier
+# notify=true
 notmuch=/usr/local/bin/notmuch
+emacsclient=/Applications/Emacs.app/Contents/MacOS/bin/emacsclient
+nm="$emacsclient -e \"(progn (call-interactively 'nm) (call-interactively 'other-frame))\""
+pid=$!
 
-function printErr {
-    $notify -message "$1" -title "Failure getting main";
-    echo "[ERROR]" $1
-    echo "$1" >&2
-}
-
-function tooBadMbsyncExit {
-    printErr "mbsync failed too many times"
-    exit 1
-}
-
-function getMail {
-    local attempts=$1
-    if [[ attempts -gt 10 ]]; then
-        tooBadMbsyncExit;
-    fi
-
-    $mbsync -V gmail || getMail $(( $attempts + 1 ))
-}
+[ -x $mbsync ] || exit 1
+killall mbsync || true
 
 function log {
-    echo "[GETMAIL]" $@
+    if [[ -x $notify ]] && [[ -n $NOTIFY ]]; then
+        $notify -title "E-Mail" -message "$@" -execute "$nm"
+    else
+        echo $@
+    fi
 }
 
-log "Getting mail..."
-getMail
+function getBox {
+    if ! $mbsync "$1"; then
+        log "Failed: $mbsync $1";
+        exit 1;
+    fi
 
-log "Indexing mail..."
-$notmuch new >& /dev/null
-unread_messages=$($notmuch count tag:unread)
+    if ! $notmuch new; then
+        log "Failed: $notmuch new";
+        exit 1;
+    fi
 
-log "Unread messages: $unread_messages"
-if [[ $unread_messages -ne 0 ]]; then
-    $notify -message "Found $unread_messages new messages" -title "New mail ($unread_messages)";
-fi
+    local new_mail=$($notmuch count tag:new);
+    if [[ $new_mail -gt 0 ]]; then
+        log "New mail: $new_mail";
+    fi
+}
+
+getBox "gmail:[Gmail]/All Mail"
+getBox gmail
